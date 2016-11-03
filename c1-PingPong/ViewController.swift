75036c2b64a00f363e397ebaebd64d7f7167e3d3
xandersung <alexandersung11@gmail.com>
2016-11-03 10:53:53 -0700

    Initial Commit

---
+++ c1-PingPong/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  c1-PingPong
+//
+//  Created by Alexander Sung on 11/3/16.
+//  Copyright © 2016 Alexander Sung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
